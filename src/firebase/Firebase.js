1badbd0710a66fe058ee298ca669c518cd7339c3
Aparnajkumar <aparnajayakumar91@gmail.com>
2026-01-20 23:04:32 +0530

    removed

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,28 +1,19 @@
-import React from 'react'
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-
-
-
 const firebaseConfig = {
-    apiKey: "AIzaSyC8uNjfh4oiv6xXJUzL_NnQuguIPSE1V7U",
-    authDomain: "login-3d2da.firebaseapp.com",
-    projectId: "login-3d2da",
-    storageBucket: "login-3d2da.firebasestorage.app",
-    messagingSenderId: "356086759342",
-    appId: "1:356086759342:web:8f2bb8ad1d8c3acfae123f",
-    measurementId: "G-405YBCJ3NS"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
-
-
-
-// Authentication
+// Auth
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
-
